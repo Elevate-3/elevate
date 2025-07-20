5aa3d44ea86fd758f699a5744050536818070070
Alex Vang <VangAlexa25@gmail.com>
2025-07-20 16:55:43 -0500

    profile reroute

--- src/views/RootView/RootView.tsx
+++ src/views/RootView/RootView.tsx
@@ -3,7 +3,15 @@ import { Outlet, Link, useNavigate } from 'react-router-dom'
 import { supabase } from '@/lib/supabase'
 import { HOME, PROFILE, SIGN_IN, GUIDE } from '@/constants/routes'
 import NavBar from '@/components/NavBar/NavBar'
-
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 
 const RootView: React.FC = () => {
   const navigate = useNavigate()
@@ -17,21 +25,33 @@ const RootView: React.FC = () => {
     }
   }
 
+  const profilePage = async () => {
+    navigate(PROFILE) // redirect to profile page
+  }
+
   return (
     <div className="min-h-screen flex flex-col">
       {/* Main layout component which will render for all children components within the <Outlet /> */}
       <header className="bg-gray-800 text-white p-4 flex justify-end items-center gap-4">
         <nav className="flex gap-4">
           <Link to={HOME}>Home</Link>
-          <Link to={PROFILE}>Profile</Link> 
           <Link to={GUIDE}>Guide</Link>
         </nav>
-        <button
-          onClick={handleSignOut}
-          className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded"
-        >
-          Sign Out
-        </button>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={profilePage}>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Settings</DropdownMenuItem>
+            <DropdownMenuItem onClick={handleSignOut}>Sign Out</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </header>
       <NavBar />
 
